0b57e34fb10f408667640c73fd362415823c9555
hivislav <hivislav@gmail.com>
2022-04-15 21:31:04 +0500

    Задание №2 (kotlin)
    //Реализован шаблон MVVM
    //Реализован основной экран для просмотра карточки фильма

---
+++ app/src/main/java/ru/kinomafia/viewmodel/MainViewModel.kt
@@ -0,0 +1,29 @@
+package ru.kinomafia.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.kinomafia.model.Repository
+import ru.kinomafia.model.RepositoryImpl
+import java.lang.Thread.sleep
+
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()) : ViewModel() {
+
+    fun getLiveData(): LiveData<AppState>{
+        return liveDataToObserve
+    }
+
+    fun getFilmInfo() = getDataFromLocalSource()
+
+
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            sleep(2000)
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getFilmInfoFromLocalStorage()))
+        }.start()
+    }
+}
