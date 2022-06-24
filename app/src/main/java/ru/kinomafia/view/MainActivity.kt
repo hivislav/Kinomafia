b8bdd581eddcdbfd3c41c590a338613f24c34527
hivislav <hivislav@gmail.com>
2022-06-24 17:20:18 +0500

    task 7
    // добавил bottom navigation

--- app/src/main/java/ru/kinomafia/view/MainActivity.kt
+++ app/src/main/java/ru/kinomafia/view/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.kinomafia.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView
 import kotlinx.android.synthetic.main.main_activity.*
 import pl.droidsonroids.gif.GifDrawable
 import ru.kinomafia.R
 import ru.kinomafia.databinding.MainActivityBinding
+import ru.kinomafia.view.favourite.FavouriteFragment
 import ru.kinomafia.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -18,16 +20,47 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.mainBottomNavigation.hide()
+
+
         val gifDrawable = GifDrawable(resources, R.drawable.start_app_animation)
         startAppAnimation.setImageDrawable(gifDrawable)
         gifDrawable.loopCount = 1
 
         gifDrawable.addAnimationListener {
             startAppAnimation.hide()
+            binding.mainBottomNavigation.show()
             if (savedInstanceState == null) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                binding.mainBottomNavigation.selectedItemId = R.id.itemBottomNavigationSearch
+            }
+        }
+
+        binding.mainBottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.itemBottomNavigationNews -> {
+                    true
+                }
+
+                R.id.itemBottomNavigationSearch -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, MainFragment.newInstance())
+                        .commitNow()
+                    true
+                }
+
+                R.id.itemBottomNavigationFavourite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, FavouriteFragment.newInstance())
+                        .commitAllowingStateLoss()
+                    true
+                }
+
+                R.id.itemBottomNavigationSettings -> {
+                    true
+                }
+                else -> {
+                    false
+                }
             }
         }
     }
